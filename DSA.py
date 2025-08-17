9b15b3105abffba4efcf61a23dc554f8c73c1f32
Hisham5092 <hishamahmed5092@gmail.com>
2025-08-18 01:03:29 +0600

    New Code

--- DSA.py
+++ DSA.py
@@ -670,3 +670,26 @@ from operator import index
 #         break
 # print(mylist)
 
+# Selection Sort
+# mylist = [54, 34, 25, 12, 22, 11, 90, 5]
+# n = len(mylist)
+# for i in range (n-1):
+#     min_index = i
+#     for j in range(i+1, n):
+#         if mylist [j] < mylist [min_index]:
+#             min_index = j
+#     min_value = mylist.pop(min_index)
+#     mylist.insert(i, min_value)
+# print(mylist)
+
+# Selection Sort Optimized
+
+mylist = [54, 34, 25, 12, 22, 11, 90, 5]
+n = len(mylist)
+for i in range (n-1):
+    min_index = i
+    for j in range(i+1, n):
+        if mylist [j] < mylist [min_index]:
+            min_index = j
+    mylist [i], mylist [min_index] = mylist [min_index], mylist [i]
+print(mylist)

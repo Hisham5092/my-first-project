119bec50d47433e2326df7c1a48bd9ceefe7e873
Hisham5092 <hishamahmed5092@gmail.com>
2025-09-09 01:16:37 +0600

    Union Cycle Detection

--- DSA.py
+++ DSA.py
@@ -1016,12 +1016,209 @@ from operator import index
 # g.dfs('D')
 
 # BFS Search Graph Class
+# class Graph:
+#     def __init__(self, size):
+#         self.adj_matrix = [[0] * size for _ in range(size)]
+#         self.size = size
+#         self.vertex_data = [''] * size
+#
+#     def add_edge(self, u, v):
+#         if 0 <= u < self.size and 0 <= v < self.size:
+#             self.adj_matrix[u][v] = 1
+#             self.adj_matrix[v][u] = 1
+#
+#     def add_vertex_data(self, vertex, data):
+#         if 0 <= vertex < self.size:
+#             self.vertex_data[vertex] = data
+#
+#     def print_graph(self):
+#         print("Adjacency Matrix:")
+#         for row in self.adj_matrix:
+#             print(' '.join(map(str, row)))
+#         print("\nVertex Data:")
+#         for vertex, data in enumerate(self.vertex_data):
+#             print(f"Vertex {vertex}: {data}")
+#
+#     def bfs(self, start_vertex_data):
+#         visited = [False] * self.size
+#         queue = [self.vertex_data.index(start_vertex_data)]
+#         visited [queue[0]] = True
+#         while queue:
+#             current_vertex = queue.pop(0)
+#             print(self.vertex_data[current_vertex], end=" ")
+#             for i in range (self.size):
+#                 if self.adj_matrix [current_vertex][i] == 1 and not visited [i]:
+#                     queue.append(i)
+#                     visited[i] = True
+# g = Graph(7)
+#
+# g.add_vertex_data(0, 'A')
+# g.add_vertex_data(1, 'B')
+# g.add_vertex_data(2, 'C')
+# g.add_vertex_data(3, 'D')
+# g.add_vertex_data(4, 'E')
+# g.add_vertex_data(5, 'F')
+# g.add_vertex_data(6, 'G')
+#
+# g.add_edge(3, 0)  # D - A
+# g.add_edge(0, 2)  # A - C
+# g.add_edge(0, 3)  # A - D
+# g.add_edge(0, 4)  # A - E
+# g.add_edge(4, 2)  # E - C
+# g.add_edge(2, 5)  # C - F
+# g.add_edge(2, 1)  # C - B
+# g.add_edge(2, 6)  # C - G
+# g.add_edge(1, 5)  # B - F
+#
+# g.print_graph()
+#
+# print("\nBreadth First Search starting from vertex D:")
+# g.bfs('D')
+
+# DFS Cycle Undirected Graph
+
+# class Graph:
+#     def __init__(self, size):
+#         self.adj_matrix = [[0] * size for _ in range(size)]
+#         self.size = size
+#         self.vertex_data = [''] * size
+#
+#     def add_edge(self, u, v):
+#         if 0 <= u < self.size and 0 <= v < self.size:
+#             self.adj_matrix[u][v] = 1
+#             self.adj_matrix[v][u] = 1
+#
+#     def add_vertex_data(self, vertex, data):
+#         if 0 <= vertex < self.size:
+#             self.vertex_data[vertex] = data
+#
+#     def print_graph(self):
+#         print("Adjacency Matrix:")
+#         for row in self.adj_matrix:
+#             print(' '.join(map(str, row)))
+#         print("\nVertex Data:")
+#         for vertex, data in enumerate(self.vertex_data):
+#             print(f"Vertex {vertex}: {data}")
+#
+#     def dfs_util(self,v,visited,parent):
+#         visited [v] = [True]
+#         for i in range(self.size):
+#             if self.adj_matrix[v][i] == 1:
+#                 if not visited[i]:
+#                     if self.dfs_util(i, visited, v):
+#                         return True
+#                 elif parent != i:
+#                     return True
+#         return False
+#
+#     def is_cyclic(self):
+#         visited =[False] * self.size
+#         for i in range(self.size):
+#             if not visited[i]:
+#                 if self.dfs_util (i, visited, -1):
+#                     return True
+#         return False
+#
+# g = Graph(7)
+#
+# g.add_vertex_data(0, 'A')
+# g.add_vertex_data(1, 'B')
+# g.add_vertex_data(2, 'C')
+# g.add_vertex_data(3, 'D')
+# g.add_vertex_data(4, 'E')
+# g.add_vertex_data(5, 'F')
+# g.add_vertex_data(6, 'G')
+#
+# g.add_edge(3, 0)  # D -> A
+# g.add_edge(0, 2)  # A -> C
+# g.add_edge(2, 1)  # C -> B
+# g.add_edge(2, 4)  # C -> E
+# g.add_edge(1, 5)  # B -> F
+# g.add_edge(4, 0)  # E -> A
+# g.add_edge(2, 6)  # C -> G
+#
+# g.print_graph()
+#
+# print("\nGraph has cycle:", g.is_cyclic())
+
+# DFS Cycle Directed Graph
+
+# class Graph:
+#     def __init__(self, size):
+#         self.adj_matrix = [[0] * size for _ in range(size)]
+#         self.size = size
+#         self.vertex_data = [''] * size
+#
+#     def add_edge(self, u, v):
+#         if 0 <= u < self.size and 0 <= v < self.size:
+#             self.adj_matrix[u][v] = 1
+#             # self.adj_matrix[v][u] = 1
+#
+#     def add_vertex_data(self, vertex, data):
+#         if 0 <= vertex < self.size:
+#             self.vertex_data[vertex] = data
+#
+#     def print_graph(self):
+#         print("Adjacency Matrix:")
+#         for row in self.adj_matrix:
+#             print(' '.join(map(str, row)))
+#         print("\nVertex Data:")
+#         for vertex, data in enumerate(self.vertex_data):
+#             print(f"Vertex {vertex}: {data}")
+#
+#     def dfs_utils(self,v,visited,recStack):
+#         visited[v] = [True]
+#         recStack[v] = [True]
+#         for i in range(self.size):
+#             if self.adj_matrix[v][i] == 1:
+#                 if not visited [i]:
+#                     if self.dfs_utils(i, visited, recStack):
+#                         return True
+#                 elif recStack[i]:
+#                     return True
+#         recStack = [False]
+#         return False
+#
+#     def is_cyclic(self):
+#         visited =[False] * self.size
+#         recStack = [False] * self.size
+#         for i in range(self.size):
+#             if not visited [i]:
+#                 if self.dfs_utils (i, visited, recStack):
+#                     return True
+#         return False
+#
+# g = Graph(7)
+#
+# g.add_vertex_data(0, 'A')
+# g.add_vertex_data(1, 'B')
+# g.add_vertex_data(2, 'C')
+# g.add_vertex_data(3, 'D')
+# g.add_vertex_data(4, 'E')
+# g.add_vertex_data(5, 'F')
+# g.add_vertex_data(6, 'G')
+#
+# g.add_edge(3, 0)  # D -> A
+# g.add_edge(0, 2)  # A -> C
+# g.add_edge(2, 1)  # C -> B
+# g.add_edge(2, 4)  # C -> E
+# g.add_edge(1, 5)  # B -> F
+# g.add_edge(4, 0)  # E -> A
+# g.add_edge(2, 6)  # C -> G
+#
+# g.print_graph()
+#
+# print("\nGraph has cycle:", g.is_cyclic())
+
+
+# Cycle Detection Union
 
 class Graph:
     def __init__(self, size):
         self.adj_matrix = [[0] * size for _ in range(size)]
         self.size = size
         self.vertex_data = [''] * size
+        self.parent = [i for i in range(size)]  # Union-Find array
 
     def add_edge(self, u, v):
         if 0 <= u < self.size and 0 <= v < self.size:
@@ -1032,25 +1229,29 @@ class Graph:
         if 0 <= vertex < self.size:
             self.vertex_data[vertex] = data
 
-    def print_graph(self):
-        print("Adjacency Matrix:")
-        for row in self.adj_matrix:
-            print(' '.join(map(str, row)))
-        print("\nVertex Data:")
-        for vertex, data in enumerate(self.vertex_data):
-            print(f"Vertex {vertex}: {data}")
+    def find(self,i):
+        if self.parent[i] == i:
+            return i
+        return self.find(self.parent[i])
 
-    def bfs(self, start_vertex_data):
-        visited = [False] * self.size
-        queue = [self.vertex_data.index(start_vertex_data)]
-        visited [queue[0]] = True
-        while queue:
-            current_vertex = queue.pop(0)
-            print(self.vertex_data[current_vertex], end=" ")
-            for i in range (self.size):
-                if self.adj_matrix [current_vertex][i] == 1 and not visited [i]:
-                    queue.append(i)
-                    visited[i] = True
+    def union(self,x,y):
+        x_root = self.find(x)
+        y_root = self.find(y)
+        print(f"Union:", self.vertex_data[x], "+", self.vertex_data[y] )
+        self.parent[x] = y_root
+        print(self.parent, '\n')
+
+
+    def is_cyclic(self):
+        for i in range(self.size):
+            for j in range (i + 1, self.size):
+                if self.adj_matrix[i][j] == 1:
+                    x = self.find(i)
+                    y = self.find(j)
+                    if x == y:
+                        return True
+                    self.union (x,y)
+            return False
 g = Graph(7)
 
 g.add_vertex_data(0, 'A')
@@ -1061,17 +1262,13 @@ g.add_vertex_data(4, 'E')
 g.add_vertex_data(5, 'F')
 g.add_vertex_data(6, 'G')
 
-g.add_edge(3, 0)  # D - A
-g.add_edge(0, 2)  # A - C
+g.add_edge(1, 0)  # B - A
 g.add_edge(0, 3)  # A - D
-g.add_edge(0, 4)  # A - E
-g.add_edge(4, 2)  # E - C
-g.add_edge(2, 5)  # C - F
-g.add_edge(2, 1)  # C - B
-g.add_edge(2, 6)  # C - G
-g.add_edge(1, 5)  # B - F
+g.add_edge(0, 2)  # A - C
+g.add_edge(2, 3)  # C - D
+g.add_edge(3, 4)  # D - E
+g.add_edge(3, 5)  # D - F
+g.add_edge(3, 6)  # D - G
+g.add_edge(4, 5)  # E - F
 
-g.print_graph()
-
-print("\nBreadth First Search starting from vertex D:")
-g.bfs('D')
+print("Graph has cycle:", g.is_cyclic())

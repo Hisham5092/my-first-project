1b178d754d164173309330b2c30e771bdf40a43a
Hisham5092 <hishamahmed5092@gmail.com>
2025-09-11 01:33:33 +0600

    Dijkstra's Algorithm

--- DSA.py
+++ DSA.py
@@ -1213,45 +1213,109 @@ from operator import index
 
 # Cycle Detection Union
 
+# class Graph:
+#     def __init__(self, size):
+#         self.adj_matrix = [[0] * size for _ in range(size)]
+#         self.size = size
+#         self.vertex_data = [''] * size
+#         self.parent = [i for i in range(size)]  # Union-Find array
+#
+#     def add_edge(self, u, v):
+#         if 0 <= u < self.size and 0 <= v < self.size:
+#             self.adj_matrix[u][v] = 1
+#             self.adj_matrix[v][u] = 1
+#
+#     def add_vertex_data(self, vertex, data):
+#         if 0 <= vertex < self.size:
+#             self.vertex_data[vertex] = data
+#
+#     def find(self,i):
+#         if self.parent[i] == i:
+#             return i
+#         return self.find(self.parent[i])
+#
+#     def union(self,x,y):
+#         x_root = self.find(x)
+#         y_root = self.find(y)
+#         print(f"Union:", self.vertex_data[x], "+", self.vertex_data[y] )
+#         self.parent[x] = y_root
+#         print(self.parent, '\n')
+#
+#
+#     def is_cyclic(self):
+#         for i in range(self.size):
+#             for j in range (i + 1, self.size):
+#                 if self.adj_matrix[i][j] == 1:
+#                     x = self.find(i)
+#                     y = self.find(j)
+#                     if x == y:
+#                         return True
+#                     self.union (x,y)
+#             return False
+# g = Graph(7)
+#
+# g.add_vertex_data(0, 'A')
+# g.add_vertex_data(1, 'B')
+# g.add_vertex_data(2, 'C')
+# g.add_vertex_data(3, 'D')
+# g.add_vertex_data(4, 'E')
+# g.add_vertex_data(5, 'F')
+# g.add_vertex_data(6, 'G')
+#
+# g.add_edge(1, 0)  # B - A
+# g.add_edge(0, 3)  # A - D
+# g.add_edge(0, 2)  # A - C
+# g.add_edge(2, 3)  # C - D
+# g.add_edge(3, 4)  # D - E
+# g.add_edge(3, 5)  # D - F
+# g.add_edge(3, 6)  # D - G
+# g.add_edge(4, 5)  # E - F
+#
+# print("Graph has cycle:", g.is_cyclic())
+
+# Dijkstra's Algorithm
+
 class Graph:
     def __init__(self, size):
         self.adj_matrix = [[0] * size for _ in range(size)]
         self.size = size
         self.vertex_data = [''] * size
-        self.parent = [i for i in range(size)]  # Union-Find array
 
-    def add_edge(self, u, v):
+    def add_edge(self, u, v, weight):
         if 0 <= u < self.size and 0 <= v < self.size:
-            self.adj_matrix[u][v] = 1
-            self.adj_matrix[v][u] = 1
+            self.adj_matrix[u][v] = weight
+            self.adj_matrix[v][u] = weight  # For undirected graph
 
     def add_vertex_data(self, vertex, data):
         if 0 <= vertex < self.size:
             self.vertex_data[vertex] = data
 
-    def find(self,i):
-        if self.parent[i] == i:
-            return i
-        return self.find(self.parent[i])
+    def dijkstra (self, start_vertex_data):
+        start_vertex = self.vertex_data.index(start_vertex_data)
+        distances = [float('inf')] * self.size
+        distances[start_vertex] = 0
+        visited = [False] * self.size
 
-    def union(self,x,y):
-        x_root = self.find(x)
-        y_root = self.find(y)
-        print(f"Union:", self.vertex_data[x], "+", self.vertex_data[y] )
-        self.parent[x] = y_root
-        print(self.parent, '\n')
+        for _ in range(self.size):
+            min_distance = float('inf')
+            u = None
+            for i in range (self.size):
+                if not visited [i] and distances[i] < min_distance:
+                    min_distance = distances[i]
+                    u = i
+            if u is None:
+                break
 
+            visited[u] = True
 
-    def is_cyclic(self):
-        for i in range(self.size):
-            for j in range (i + 1, self.size):
-                if self.adj_matrix[i][j] == 1:
-                    x = self.find(i)
-                    y = self.find(j)
-                    if x == y:
-                        return True
-                    self.union (x,y)
-            return False
+            for v in range (self.size):
+                if self.adj_matrix [u][v] != 0  and not visited [v]:
+                    alt = distances[u] + self.adj_matrix [u][v]
+                    if alt < distances [v]:
+                        distances[v] = alt
+
+        return distances
+
 g = Graph(7)
 
 g.add_vertex_data(0, 'A')
@@ -1262,13 +1326,23 @@ g.add_vertex_data(4, 'E')
 g.add_vertex_data(5, 'F')
 g.add_vertex_data(6, 'G')
 
-g.add_edge(1, 0)  # B - A
-g.add_edge(0, 3)  # A - D
-g.add_edge(0, 2)  # A - C
-g.add_edge(2, 3)  # C - D
-g.add_edge(3, 4)  # D - E
-g.add_edge(3, 5)  # D - F
-g.add_edge(3, 6)  # D - G
-g.add_edge(4, 5)  # E - F
+g.add_edge(3, 0, 4)  # D - A, weight 5
+g.add_edge(3, 4, 2)  # D - E, weight 2
+g.add_edge(0, 2, 3)  # A - C, weight 3
+g.add_edge(0, 4, 4)  # A - E, weight 4
+g.add_edge(4, 2, 4)  # E - C, weight 4
+g.add_edge(4, 6, 5)  # E - G, weight 5
+g.add_edge(2, 5, 5)  # C - F, weight 5
+g.add_edge(2, 1, 2)  # C - B, weight 2
+g.add_edge(1, 5, 2)  # B - F, weight 2
+g.add_edge(6, 5, 5)  # G - F, weight 5
 
-print("Graph has cycle:", g.is_cyclic())
+# Dijkstra's algorithm from D to all vertices
+print("Dijkstra's Algorithm starting from vertex D:\n")
+distances = g.dijkstra('D')
+for i, d in enumerate(distances):
+    print(f"Shortest distance from D to {g.vertex_data[i]}: {d}")
+
+
+
+
